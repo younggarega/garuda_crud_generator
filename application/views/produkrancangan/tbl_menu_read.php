d1eb928458b526be536ac880e2d5d6e306920fb9
zaka <zakamursydan@gmail.com>
2018-08-08 10:10:07 +0700

    read

---
+++ application/views/produkrancangan/tbl_menu_read.php
@@ -0,0 +1,21 @@
+  <!-- Main content -->
+  <div class="content-wrapper">
+        <section class='content'>
+          <div class='row'>
+            <div class='col-xs-12'>
+              <div class='box'>
+                <div class='box-header'>
+                <h3 class='box-title'>Produk Rancangan Read</h3>
+        <table class="table table-bordered">
+      <tr><td>Id Produk</td><td><?php echo $id_produk; ?></td></tr>
+        <tr><td>Nama Produk</td><td><?php echo $nama_produk; ?></td></tr>
+        <tr><td>Nama Komponen</td><td><?php echo $id_komponen; ?></td></tr>
+        <tr><td>Jumlah Komponen</td><td><?php echo $jml_komponen; ?></td></tr>
+        <tr><td></td><td><a href="<?php echo site_url('produkrancangan') ?>" class="btn btn-default">Cancel</a></td></tr>
+    </table>
+        </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </section><!-- /.content -->
+        </div>
